fab3c924334fc8b0ec1676d64a0bc56ec06e7b0e
geoffreylgv <evanomarkez@gmail.com>
2021-09-09 11:53:06 +0000

    f(x)=zero updated

--- c/fxegalzero/fxoall.c
+++ c/fxegalzero/fxoall.c
@@ -59,3 +59,136 @@ int controlEntierN(char *message)
     return n;
 }
 
+
+/* @dichotomie la méthode de dichotomieaffiche tout ce qui a été saisie par l'utulisateur en faite le système matriciel
+*/
+void dichotomie(float a, float b, int n)
+{
+    double erreur = fabs(n);
+    erreur = 1/pow(10,n);
+    float m,inf,sup;
+    int i=1000, trouve=0, it=0;
+    if(!fonction(a) && !fonction(b))
+    {
+        if (!fonction(a))
+            printf("\nLa solution est l'intervalle [%f ; %f]",a,b);
+        else
+            printf("\nL'équation n'admet pas de solution");
+    }
+    else
+    {
+        if(fonction(a)*fonction(b)<0)
+        {
+            m= (fonction(a) + fonction(b))/2; /// formule de Ck Dichotomie
+            inf = a;
+            sup=b;
+            while(trouve== 0 && i>0 && erreur< (sup-inf))
+            {
+                it++;
+
+                if (fonction(inf)*fonction(m)<0)
+                {
+                    sup=m;
+                    m= (fonction(inf) + fonction(sup))/2; /// formule de Ck Dichotomie
+                    printf("\nL'iteration N° %d => [%fl, %fl] ", it, inf, sup);
+                }
+                else if (fonction(inf)*fonction(m)>0)
+                {
+                    inf= m;
+                    m=(fonction(inf) + fonction(sup))/2; /// formule de Ck Dichotomie
+                    printf("\nL'iteration N° %d => [%fl, %fl] ", it, inf, sup);
+                }
+                else
+                {
+                    printf("\nL'iteration N° %d ", it);
+                    if (!fonction(inf))
+                        printf("\nLa solution est x= %f",inf);
+                    if(!fonction(m))
+                        printf("\nLa solution est x= %f",m);
+                    trouve=1;
+                }
+                i--;
+            }
+            if(!trouve)
+                printf("\nLa solution :  x = %f ",sup);
+        }
+        else if (fonction(a)*fonction(b)>0)
+        {
+            ///printf("\n Pas de solution dans cet intervalle");
+            balayage(a,b,n);
+        }
+        else
+        {
+            if(fonction(a)==0)
+                printf("\nLa solution est x= %.0f",a);
+            if (fonction(b)==0)
+                printf("\nLa solution est x= %.0f",b);
+        }
+    }
+
+}
+
+void lagrange(float a, float b, int n)
+{
+    double erreur;
+    n = fabs(n);
+    erreur = 1/pow(10,n);
+    float m,inf,sup;
+    int i=1000, it=0, trouve=0;
+    if(!fonction(a) && !fonction(b))
+    {
+        if (!fonction(a))
+            printf("\nL'intervalle [%f ; %f] est solution",a,b);
+        else
+            printf("\nL'équation n'admet pas de solution");
+    }
+    else
+    {
+        if(fonction(a)*fonction(b)<0)
+        {
+            m= a - (fonction(a)*(b-a)/(fonction(b)-fonction(a))); /// formule de Ck Lagrange
+            inf = a;
+            sup=b;
+            while(trouve== 0 && i>0 && erreur< (sup-inf))
+            {
+                if (fonction(inf)*fonction(m)<0)
+                {
+                    sup=m;
+                    m=inf - (fonction(inf)*(sup - inf))/(fonction(sup)-fonction(inf));/// formule de Ck Lagrange
+                    printf("\nL'iteration N° %d => [%fl, %fl] ", it, inf, sup);
+                }
+                else if (fonction(inf)*fonction(m)>0)
+                {
+                    inf= m;
+                    m=inf - (fonction(inf)*(sup - inf))/(fonction(sup)-fonction(inf));/// formule de Ck Lagrange
+                    printf("\nL'iteration N° %d => [%fl, %fl] ", it, inf, sup);
+                }
+                else
+                {
+                    if (!fonction(inf))
+                        printf("\nLa solution est x= %f",inf);
+                    if(!fonction(m))
+                        printf("\nLa solution est x= %f",m);
+                    trouve=1;
+                }
+                i--;
+            }
+            if(!trouve)
+                printf("\nLa solution :  x = %f ",sup);
+        }
+        else if (fonction(a)*fonction(b)>0)
+        {
+            balayage(a,b,n);
+            ///printf("\n Pas de solution dans cet intervalle");
+        }
+        else
+        {
+            if(fonction(a)==0)
+                printf("\nLa solution est x= %.0f",a);
+            if (fonction(b)==0)
+                printf("\nLa solution est x= %.0f",b);
+        }
+    }
+}
+
+
